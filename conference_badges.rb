fb39f843cacdcef57f69ff94df1e1e668b5b04cb
Joe Ganobsik <jganobsik@gmail.com>
2018-12-18 22:24:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,10 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  badge_array = []
   array.each {|name|
-  badge_maker(name)
+  nametag = badge_maker(name)
+  badge_array.push(nametag)
   }
+  return badge_array
 end
