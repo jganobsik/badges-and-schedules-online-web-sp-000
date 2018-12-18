bab663a2fe03f5316ed10620b0b1fcdce4d7d64a
Joe Ganobsik <jganobsik@gmail.com>
2018-12-18 22:38:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,8 +18,9 @@ def assign_rooms(speakers)
   room_assignments = []
   speakers.each_with_index  do |speaker, index| 
     room_string = room_counter.to_s
-    speakers[speaker] = index 
-    room_assignments.push("Hello, #{speakers[speaker] = index}! You'll be assigned to room #{room_counter}!")
+    speakers[speaker] = index
+
+    room_assignments.push("Hello, #{speakers}! You'll be assigned to room #{room_counter}!")
     room_counter += 1 
   end
   return room_assignments
